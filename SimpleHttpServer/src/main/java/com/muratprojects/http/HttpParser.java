8744c6166b568fbdcf2057c5be8f5059e3f35208
Kapparov Murat <paprim2003@gmail.com>
2024-07-04 21:40:44 +0500

    Update HttpParser.java

--- SimpleHttpServer/src/main/java/com/muratprojects/http/HttpParser.java
+++ SimpleHttpServer/src/main/java/com/muratprojects/http/HttpParser.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 public class HttpParser {
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
 
-
     public void parseHttpRequest(InputStream inputStream){
         parseRequestLine(inputStream);
         parseHeaders(inputStream);
